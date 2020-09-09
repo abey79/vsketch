30f627e27b0ec85807b9fae0b46c00f12659a596
Loic <loic.goulefert31@gmail.com>
2020-09-09 17:55:37 +0200

    Added more tests for circle()

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,22 +1,13 @@
+from typing import Tuple
+
 import pytest
+
+import vsketch
 
 from .utils import bounds_equal, line_count_equal
 
 
-def test_circle_radius(vsk):
-    vsk.circle(0, 0, radius=5)
-    assert line_count_equal(vsk, 1)
-    assert bounds_equal(vsk, -5, -5, 5, 5)
-
-
-def test_circle_diameter(vsk):
-    vsk.detail(0.01)  # make sure we have a tight bound match
-    vsk.circle(0, 0, diameter=5)
-    assert line_count_equal(vsk, 1)
-    assert bounds_equal(vsk, -2.5, -2.5, 2.5, 2.5)
-
-
-def test_circle_default(vsk):
+def test_circle_default(vsk: vsketch.Vsketch) -> None:
     # default should be diameter
     vsk.detail(0.01)  # make sure we have a tight bound match
     vsk.circle(0, 0, 5)
@@ -24,8 +15,45 @@ def test_circle_default(vsk):
     assert bounds_equal(vsk, -2.5, -2.5, 2.5, 2.5)
 
 
-def test_circle_bad_args(vsk):
+def test_circle_radius(vsk: vsketch.Vsketch) -> None:
+    vsk.circle(0, 0, radius=5)
+    assert line_count_equal(vsk, 1)
+    assert bounds_equal(vsk, -5, -5, 5, 5)
+
+
+def test_circle_diameter(vsk: vsketch.Vsketch) -> None:
+    vsk.detail(0.01)  # make sure we have a tight bound match
+    vsk.circle(0, 0, diameter=5)
+    assert line_count_equal(vsk, 1)
+    assert bounds_equal(vsk, -2.5, -2.5, 2.5, 2.5)
+
+
+@pytest.mark.parametrize(
+    ["data", "mode", "expected"],
+    [
+        [(0, 0, 5), "center", (-2.5, -2.5, 2.5, 2.5)],
+        [(3, 3, 1), "radius", (2, 2, 4, 4)],
+        [(3, 3, 1), "corner", (3, 3, 4, 4)],
+        # 'corners' == 'corner' for circle()
+        [(3, 3, 1), "corners", (3, 3, 4, 4)],
+    ],
+)
+def test_circle_mode(
+    vsk: vsketch.Vsketch,
+    data: Tuple[float, float, float],
+    mode: str,
+    expected: Tuple[float, float, float, float],
+) -> None:
+    vsk.detail(0.01)
+    vsk.circle(*data, mode=mode)
+    assert line_count_equal(vsk, 1)
+    assert bounds_equal(vsk, *expected)
+
+
+def test_circle_bad_args(vsk: vsketch.Vsketch) -> None:
     with pytest.raises(ValueError):
-        vsk.circle(0, 0)
+        vsk.circle(0, 0)  # type: ignore
     with pytest.raises(ValueError):
         vsk.circle(0, 0, radius=10, diameter=20)
+    with pytest.raises(ValueError):
+        vsk.circle(2, 2, 5, mode="jumbo")
