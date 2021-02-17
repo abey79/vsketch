4602e815774e66c10837ea65a14411b33d801723
Antoine Beyeler <abeyeler@ab-ware.com>
2021-02-17 10:56:04 +0100

    Fixed windows error

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,4 +1,3 @@
-import glob
 import pathlib
 
 import nbformat
@@ -7,14 +6,14 @@ from nbconvert.preprocessors import ExecutePreprocessor
 from typer.testing import CliRunner
 from vsketch_cli.cli import cli
 
-EXAMPLES = str(pathlib.Path(__file__).parent.parent.absolute()) + "/examples/"
-NOTEBOOKS = EXAMPLES + "_notebooks/"
+EXAMPLES = pathlib.Path(__file__).parent.parent.absolute() / "examples"
+NOTEBOOKS = EXAMPLES / "_notebooks"
 
 runner = CliRunner()
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("ipynb", glob.glob(NOTEBOOKS + "*.ipynb"))
+@pytest.mark.parametrize("ipynb", NOTEBOOKS.glob("*.ipynb"))
 def test_example_notebooks(tmp_path, ipynb):
     with open(ipynb) as f:
         nb = nbformat.read(f, as_version=4)
@@ -22,8 +21,9 @@ def test_example_notebooks(tmp_path, ipynb):
     ep.preprocess(nb, {"metadata": {"path": tmp_path}})
 
 
-@pytest.mark.parametrize("path", glob.glob(EXAMPLES + "[!_]*[!.md]"))
+@pytest.mark.parametrize("path", EXAMPLES.glob("[!_]*[!.md]"))
 def test_examples(tmp_path, path):
-    res = runner.invoke(cli, f"save --name test_output --destination {tmp_path} {path}")
+    # Note: split is needed to avoid `invoke`'s preprocessor to eat Windows' path backslashes
+    res = runner.invoke(cli, f"save --name test_output --destination {tmp_path} {str(path)}".split())
     assert res.exit_code == 0
     assert (pathlib.Path(tmp_path) / "test_output.svg").exists()
