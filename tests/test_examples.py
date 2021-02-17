e962c10da9bc8935f77673104cc11d6aa9e7fcf7
Antoine Beyeler <abeyeler@ab-ware.com>
2021-02-17 11:01:45 +0100

    Another fix for hidden files

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -21,9 +21,11 @@ def test_example_notebooks(tmp_path, ipynb):
     ep.preprocess(nb, {"metadata": {"path": tmp_path}})
 
 
-@pytest.mark.parametrize("path", EXAMPLES.glob("[!_]*[!.md]"))
+@pytest.mark.parametrize("path", EXAMPLES.glob("[!._]*[!.md]"))
 def test_examples(tmp_path, path):
     # Note: split is needed to avoid `invoke`'s preprocessor to eat Windows' path backslashes
-    res = runner.invoke(cli, f"save --name test_output --destination {tmp_path} {str(path)}".split())
+    res = runner.invoke(
+        cli, f"save --name test_output --destination {tmp_path} {str(path)}".split()
+    )
     assert res.exit_code == 0
     assert (pathlib.Path(tmp_path) / "test_output.svg").exists()
